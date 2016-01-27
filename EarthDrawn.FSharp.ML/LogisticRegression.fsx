2026ddb7d1ae827d0b5ce96610b68dfd111b9fe2
Andrew <andrewthart098@gmail.com>
2016-01-27 11:59:04 -0500

    Added MathNet.Numerics.FSharp

--- EarthDrawn.FSharp.ML/LogisticRegression.fsx
+++ EarthDrawn.FSharp.ML/LogisticRegression.fsx
@@ -1,16 +1,15 @@
 ﻿#r @"..\packages\FSharp.Data.2.2.5\lib\net40\FSharp.Data.dll"
-#r @"..\packages\MathNet.Numerics.3.10.0\lib\net40\MathNet.Numerics.dll"
+#r @"..\packages\MathNet.Numerics.FSharp.3.10.0\lib\net40\MathNet.Numerics.FSharp.dll"
 
 open System
 open FSharp.Data
 open MathNet.Numerics.LinearAlgebra
-open MathNet.Numerics.LinearAlgebra.Double
 
 // ********************************    
 // BUILDING MATRICIES
 // ********************************
-let M = Matrix<float>.Build
-let V = Vector<float>.Build
+//let M = Matrix<float>.Build
+//let V = Vector<float>.Build
 
 // Turn the array of string[] to an array of float[]
 // TODO: Take into account cultural differences of floats
@@ -24,7 +23,7 @@ let readData (path : string) =
                 |> Seq.map (fun x -> x.Columns) 
                 |> Seq.map (fun x -> cast2float x)
                 |> array2D
-    M.DenseOfArray f2d
+    matrix f2d
 
 
 // ********************************
@@ -34,9 +33,10 @@ let readData (path : string) =
 let sigmoid z = 1.0 / (1.0 + exp -z)
 
 let cost (y:Matrix) (X:Matrix<float>) (theta:Matrix<float>) = 
-    let h = (X.Transpose()*theta).Enumerate() 
-                |> Seq.map sigmoid
-    let firstTerm = -y*log(h)
+    let h = (X.Transpose()*theta) |> Matrix.map(fun x -> log(sigmoid x))
+    let firstTerm = -y*h 
+    let secondTern = (1-y)*h
+    (1/(float X.RowCount))*firstTern*secondTerm |> Matrix.
 
 //let logReg (X:Matrix<float>) (y:Matrix<float>)=
 //    (X.Transpose()*X).Inverse()*X.Transpose()*y
@@ -53,8 +53,17 @@ let theta = M.Dense(X.RowCount, 1, 0.0)
 
 let h = (X.Transpose()*theta).Enumerate() 
                 |> Seq.map sigmoid
-                |> Seq.toArray
-                |> DenseMatrix.OfRowArrays
                 
                 
 let firstTerm = -y*log(h)
+
+
+
+let csv = CsvFile.Load(@"C:\Users\andre\Source\OSS\EarthDrawn.FSharp.ML\TestingData\LogisitcRegression\ex2data1.csv")
+let f2d = csv.Rows 
+            |> Seq.map (fun x -> x.Columns) 
+
+let h = (X.Transpose()*theta).Enumerate() 
+                |> Seq.map sigmoid
+
+let rArray = f2d |> Seq.map(fun x -> string[] x) |> array2D
---
+++ EarthDrawn.FSharp.ML/NeuralNet.fsx
@@ -0,0 +1,10 @@
+﻿
+let Jo (theta: float) = 2.0*(theta**4.0)+2.0
+
+let theta = 1.0
+let e = 0.01
+
+let theta1 = theta + e
+let theta2 = theta - e
+
+let res = ((Jo theta1) - (Jo theta2))/(2.0*e)
