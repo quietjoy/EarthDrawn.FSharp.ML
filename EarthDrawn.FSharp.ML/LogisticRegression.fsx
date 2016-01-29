248e537acea79ee3a6207005db1522099dafa0e9
Andrew <andrewthart098@gmail.com>
2016-01-28 19:50:35 -0500

    Good start on gradient descent algorithm for logistic regression

--- EarthDrawn.FSharp.ML/LogisticRegression.fsx
+++ EarthDrawn.FSharp.ML/LogisticRegression.fsx
@@ -1,11 +1,18 @@
 ﻿#r @"..\packages\FSharp.Data.2.2.5\lib\net40\FSharp.Data.dll"
 #r @"..\packages\MathNet.Numerics.3.10.0\lib\net40\MathNet.Numerics.dll"
+#r @"..\packages\DotNumerics.1.1\lib\DotNumerics.dll"
+
 
 #load "..\packages\MathNet.Numerics.FSharp.3.10.0\MathNet.Numerics.fsx"
 
 open System
 open FSharp.Data
 open MathNet.Numerics.LinearAlgebra
+open DotNumerics
+
+// TODO
+// Implement Gradient descent
+// Add Regularization
 
 // ********************************    
 // BUILDING MATRICIES
@@ -31,24 +38,32 @@ let readData (path : string) =
 // ********************************
 let sigmoid z = 1.0 / (1.0 + exp -z)
 
-let computeCost (y:Matrix<float>) (X:Matrix<float>) (theta:Matrix<float>) = 
+let computeCost (y:Matrix<float>) (X:Matrix<float>) (theta:Matrix<float>) (lambda: float) = 
     let m = (float X.RowCount)
     let h = (X*theta) |> Matrix.map(fun x -> log(sigmoid x))
     let h1 = (X*theta) |> Matrix.map(fun x -> log(1.0-sigmoid x)) 
     let sum = ((-y.*h) - ((y |> Matrix.map (fun x -> x-1.0)).*h1) |> Matrix.sum)
-    (1.0/(float X.RowCount))*sum
+    let J = (1.0/(float X.RowCount))*sum
+    J + (lambda/(2.0*m))*(theta |> Matrix.sum)
 
-let gradient (y:Matrix<float>) (X:Matrix<float>) (theta:Matrix<float>) =
+let computeGradient (y:Matrix<float>) (X:Matrix<float>) (theta:Matrix<float>) (alpha: float) =
     let m = (float X.RowCount)
     let hx = (X*theta) |> Matrix.map(fun x -> sigmoid x)
     let h = hx-y
     let sum = X |> Matrix.mapRows (fun i row -> h.[i, 0]*row) |> Matrix.sumCols 
-    (1.0/m)*sum
+    alpha*(1.0/m)*sum
 
 let costFunction (y:Matrix<float>) (X:Matrix<float>) (theta:Matrix<float>) = 
     let t1f = computeCost y X theta
-    let t2f = gradient y X theta
+    let t2f = computeGradient y X theta
     (t1f, t2f)
+
+let performGradientDescent (y:Matrix<float>) (X:Matrix<float>) (theta:Matrix<float>)
+                           (lambda: float) (alpha: float) (iterations: int) =
+    let gradient = 
+    let J = [| for i in 1 .. iterations -> computeCost y X theta lamda |]
+        
+    
 
 // ********************************
 // VARIABLES
@@ -60,5 +75,14 @@ let X = Matrix.Build.Dense(data.RowCount, 1, 1.0)
                 .Append(data.RemoveColumn(data.ColumnCount-1))
 let y = Matrix.Build.DenseOfColumnVectors(data.Column(data.ColumnCount-1))
 let theta = Matrix.Build.Dense(X.ColumnCount, 1, 0.0)
+let δ = System.Double.Epsilon ** (1.0 / 3.0)
 
-costFunction X y theta
+let lamda = 1.0
+let g, theta_final = costFunction y X theta
+
+
+//let bfgsMin (initialGuess: float[]) costFunction gradientFunction =
+//    let lbfgsb = DotNumerics.Optimization.L_BFGS_B()
+//    lbfgsb.ComputeMin(costFunction, gradientFunction, initialGuess);
+
+let J = [| for i in 1 .. 100 -> computeCost y X theta lamda |]
